c3761cb7d6b6e3e06c1adbf163d53077d5eab537
Ifeanyi Nnamdi-Okagbue <ifeanyi.okagbue@gmail.com>
2023-11-18 21:24:05 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ifeanyi_algorithm_solutions.algorithms.arrays;
+
+Console.WriteLine("");
+
+//Implementation of Two Sum and Two Sum Boolean
+int[] nums = new int[] { 2, 3, 4, 5 };
+//var result = two_sum.two_sum_solution(nums, 6);
+//foreach(int res in result)
+//{
+//    Console.Write(res);
+//}
+Console.WriteLine(two_sum.two_sum_solution_bool(nums, 6).ToString());
+
+
+Console.WriteLine("");
+
+
---
+++ algorithms/arrays/two_sum.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifeanyi_algorithm_solutions.algorithms.arrays
+{
+    public class two_sum
+    {
+        public static int[] two_sum_solution(int[] nums, int target)
+        {
+            int[] returnValue = new int[2];
+            var numbers1 = new HashSet<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int isEqual = target - nums[i];
+
+                if (numbers1.Contains(isEqual))
+                {
+                    returnValue[0] = Array.IndexOf(nums, isEqual);
+                    returnValue[1] = i;
+                    break;
+                }
+                else
+                {
+                    numbers1.Add(nums[i]);
+                }
+            }
+
+            return returnValue;
+        }
+
+        public static bool two_sum_solution_bool(int[] nums, int target)
+        {
+            bool returnValue = false;
+            var numbers1 = new HashSet<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int isEqual = target - nums[i];
+
+                if (numbers1.Contains(isEqual))
+                {
+                    returnValue = true;
+                    break;
+                }
+                else
+                {
+                    numbers1.Add(nums[i]);
+                }
+            }
+
+            return returnValue;
+        }
+    }
+}
