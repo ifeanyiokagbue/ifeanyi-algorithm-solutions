9c08a5e22c7e592a12f4e64a29a28de8aff217b5
Ifeanyi Nnamdi-Okagbue <ifeanyi.okagbue@gmail.com>
2023-11-21 08:08:11 +0100

    Best time to sell stock #1

--- Program.cs
+++ Program.cs
@@ -4,14 +4,17 @@ using ifeanyi_algorithm_solutions.algorithms.arrays;
 Console.WriteLine("");
 
 //Implementation of Two Sum and Two Sum Boolean
-int[] nums = new int[] { 2, 3, 4, 5 };
+//int[] nums = new int[] { 2, 3, 4, 5 };
 //var result = two_sum.two_sum_solution(nums, 6);
 //foreach(int res in result)
 //{
 //    Console.Write(res);
 //}
-Console.WriteLine(two_sum.two_sum_solution_bool(nums, 6).ToString());
+//Console.WriteLine(two_sum.two_sum_solution_bool(nums, 6).ToString());
 
+//Implementation of Max Profit
+int[] nums = new int[] { 2, 6, 3, 11 };
+Console.WriteLine($"The maximum profit is {max_profit.MaxProfit(nums)}");
 
 Console.WriteLine("");
 
---
+++ algorithms/arrays/max_profit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifeanyi_algorithm_solutions.algorithms.arrays
+{
+    public class max_profit
+    {
+        public static int MaxProfit(int[] prices)
+        {
+            int minPrice = int.MaxValue;
+            int maxProfit = 0;
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if (prices[i] < minPrice)
+                {
+                    minPrice = prices[i];
+                }
+                else if (prices[i] - minPrice > maxProfit)
+                {
+                    maxProfit = prices[i] - minPrice;
+                }
+            }
+
+            return maxProfit;
+        }
+    }
+}
